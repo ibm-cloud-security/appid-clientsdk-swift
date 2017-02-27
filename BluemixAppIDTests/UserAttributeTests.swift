2e874744c463a521addc08481cd64aa9f70a5599
MOTYD@il.ibm.com <motyd@il.ibm.com>
2017-02-27 15:34:11 +0200

    fix comments
    fix comments in top of file and minor changes

--- BluemixAppIDTests/UserAttributeTests.swift
+++ BluemixAppIDTests/UserAttributeTests.swift
@@ -1,10 +1,14 @@
-//
-//  UserAttributeTests.swift
-//  BluemixAppID
-//
-//  Created by Moty Drimer on 23/02/2017.
-//  Copyright © 2017 Oded Betzalel. All rights reserved.
-//
+/* *     Copyright 2016, 2017 IBM Corp.
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
 
 import Foundation
 import XCTest
@@ -41,6 +45,7 @@ public class UserAttributeTests: XCTestCase {
         
         var failed = false;
         var passed = false;
+        var failureDesc : String? = nil
         
         func onSuccess(result: [String:Any]) {
             XCTAssert(result["key"] != nil)
@@ -50,6 +55,7 @@ public class UserAttributeTests: XCTestCase {
             passed = true
         }
         func onFailure(error: UserAttributeError) {
+            failureDesc = error.description
             failed = true
         }
         
@@ -155,6 +161,21 @@ public class UserAttributeTests: XCTestCase {
         if delegate.failed || !delegate.passed {
             XCTFail()
         }
+        delegate.reset()
+        
+        
+    }
+    
+    func testFailure () {
+        var delegate = MyDelegate()
+        var userAttributeManager = MockUserAttributeManger(appId: AppID.sharedInstance)
+        var resp = HTTPURLResponse(url: URL(string: "http://someurl.com")!, statusCode: 404, httpVersion: "1.1", headerFields: [:])
+        userAttributeManager.response = resp
+        userAttributeManager.data = "{\"error\" : \"NOT_FOUND\"}".data(using: .utf8)
+        userAttributeManager.token = "token"
+        userAttributeManager.expectMethod = "PUT"
+        userAttributeManager.setAttribute(key : "key", value : "value", accessTokenString: "token", delegate: delegate)
+        XCTAssert(delegate.failed)
         delegate.reset()
         
         

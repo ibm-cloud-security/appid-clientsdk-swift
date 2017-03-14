46ebee5b5659afd638dee21bcde2e35d556420b5
Kate Lyons <knlyons@us.ibm.com>
2017-03-14 16:00:13 -0500

    Fixed spacing of product name to App ID.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# This script is used by Travis-CI to automatically release new versions of AppID.
+# This script is used by Travis-CI to automatically release new versions of App ID.
 # First, we check if the version specified in the .podspec does not already exist as a git tag.
 # If the version does not exist yet, we add a git tag for this new version and publish to Cocoapods.
 
 set -ev
 cd ~/Documents
-# GITHUB_TOKEN required for Travis to have permissions to push to the AppID repository
+# GITHUB_TOKEN required for Travis to have permissions to push to the App ID repository
 git clone https://ibm-bluemix-mobile-services:${GITHUB_TOKEN}@github.com/ibm-bluemix-mobile-services/appid-clientsdk-swift.git
 cd appid-clientsdk-swift
 git remote rm origin

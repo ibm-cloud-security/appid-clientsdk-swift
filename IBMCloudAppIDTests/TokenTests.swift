90e8ddc433d6c3695d34cd1bf4a8e92fba499d8b
gtaban <gtaban@us.ibm.com>
2019-04-04 19:27:31 -0500

    master <-- Development (#175)
    * update login widget section in readme
    
    * changing initializer to public (#171)
    
    * Point to v4 (#173)
    
    * Update to point to v4 + iss and aud validation
     Adds azp and issuer validation for V4
    
    * Issue 2439 (#174)
    
    Removed azp validation and updated tokens to v4 for tests
    
    * bump up version to 6.0.0

--- IBMCloudAppIDTests/TokenTests.swift
+++ IBMCloudAppIDTests/TokenTests.swift
@@ -18,21 +18,21 @@ class TokenTests: XCTestCase {
     func testValidAccessToken() {
         let token = AccessTokenImpl(with: AppIDTestConstants.ACCESS_TOKEN)
         XCTAssertNotNil(token)
-        XCTAssertEqual(token?.scope, "appid_default appid_readprofile appid_readuserattr appid_writeuserattr")
+        XCTAssertEqual(token?.scope, "openid appid_default appid_readprofile appid_readuserattr appid_writeuserattr appid_authenticated")
         XCTAssertEqual(token?.raw, AppIDTestConstants.ACCESS_TOKEN)
         XCTAssertNotNil(token?.header)
         XCTAssertNotNil(token?.payload)
         XCTAssertNotNil(token?.signature)
-        XCTAssertEqual(token?.issuer, "mobileclientaccess.stage1.ng.bluemix.net")
-
-        XCTAssertNil(token?.subject)
-        XCTAssertEqual(token?.audience, "26cb012eb327c612d90a6819163b6bcbd4849cbb")
-        XCTAssertTrue(token?.issuedAt == Date(timeIntervalSince1970: 1487081278 as Double))
-        XCTAssertEqual(token?.tenant, "4dba9430-54e6-4cf2-a516-6f73feb702bb")
-        XCTAssertEqual(token?.authenticationMethods?[0], nil)
+        XCTAssertEqual(token?.issuer, AppIDTestConstants.region + "/oauth/v4/" + AppIDTestConstants.tenantId)
+        
+        XCTAssertEqual(token?.subject, AppIDTestConstants.subject)
+        XCTAssertEqual(token?.audience, [AppIDTestConstants.clientId])
+        XCTAssertTrue(token?.issuedAt == Date(timeIntervalSince1970: 1552502422 as Double))
+        XCTAssertEqual(token?.tenant, AppIDTestConstants.tenantId)
+        XCTAssertEqual(token?.authenticationMethods?[0], "google")
         XCTAssertTrue(token!.isExpired)
         XCTAssertFalse(token!.isAnonymous)
-        XCTAssertTrue(token?.expiration == Date(timeIntervalSince1970: 1487084878 as Double))
+        XCTAssertTrue(token?.expiration == Date(timeIntervalSince1970: 1552502424 as Double))
 
     }
 
@@ -41,24 +41,22 @@ class TokenTests: XCTestCase {
         let token = IdentityTokenImpl(with: AppIDTestConstants.ID_TOKEN)
 
         XCTAssertEqual(token?.email, "donlonqwerty@gmail.com")
-        XCTAssertEqual(token?.gender, "male")
-        XCTAssertEqual(token?.locale, "en_US")
-        XCTAssertEqual(token?.name, "Don Lon")
-        XCTAssertEqual(token?.picture, "https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/13501551_286407838378892_1785766211766730697_n.jpg?oh=242bc2fb505609b442874fde3e9865a9&oe=5907B1BC")
-        XCTAssertEqual(token?.identities?.count,1)
+        XCTAssertNil(token?.gender)
+        XCTAssertEqual(token?.locale, "en")
+        XCTAssertEqual(token?.name, "Lon Don")
         XCTAssertEqual(token?.raw, AppIDTestConstants.ID_TOKEN)
         XCTAssertNotNil(token?.header)
         XCTAssertNotNil(token?.payload)
         XCTAssertNotNil(token?.signature)
-        XCTAssertEqual(token?.issuer, "mobileclientaccess.stage1.ng.bluemix.net")
+        XCTAssertEqual(token?.issuer, AppIDTestConstants.region + "/oauth/v4/" + AppIDTestConstants.tenantId)
 
-        XCTAssertNil(token?.subject)
-        XCTAssertEqual(token?.audience, "26cb012eb327c612d90a6819163b6bcbd4849cbb")
-        XCTAssertTrue(token?.issuedAt == Date(timeIntervalSince1970: 1487081278 as Double))
-        XCTAssertEqual(token?.tenant, "4dba9430-54e6-4cf2-a516-6f73feb702bb")
-        XCTAssertEqual(token?.authenticationMethods?[0], nil)
+        XCTAssertEqual(token?.subject, AppIDTestConstants.subject)
+        XCTAssertEqual(token?.audience, [AppIDTestConstants.clientId])
+        XCTAssertTrue(token?.issuedAt == Date(timeIntervalSince1970: 1552502422 as Double))
+        XCTAssertEqual(token?.tenant, AppIDTestConstants.tenantId)
+        XCTAssertEqual(token?.authenticationMethods?[0], "google")
         XCTAssertTrue(token!.isExpired)
-        XCTAssertTrue(token?.expiration == Date(timeIntervalSince1970: 1487084878 as Double))
+        XCTAssertTrue(token?.expiration == Date(timeIntervalSince1970: 1552502424 as Double))
 
     }
 
--- Source/IBMCloudAppID/internal/tokens/AbstractToken.swift
+++ Source/IBMCloudAppID/internal/tokens/AbstractToken.swift
@@ -1,86 +1,86 @@
 import Foundation
 
 public protocol Token {
-
-	var raw: String {get}
-	var header: Dictionary<String, Any> {get}
-	var payload: Dictionary<String, Any> {get}
-	var signature: String {get}
-	
-	var issuer: String? {get}
-	var subject: String? {get}
-	var audience: String? {get}
-	var expiration: Date? {get}
-	var issuedAt: Date? {get}
-	var tenant: String? {get}
+    
+    var raw: String {get}
+    var header: Dictionary<String, Any> {get}
+    var payload: Dictionary<String, Any> {get}
+    var signature: String {get}
+    
+    var issuer: String? {get}
+    var subject: String? {get}
+    var audience: [String]? {get}
+    var expiration: Date? {get}
+    var issuedAt: Date? {get}
+    var tenant: String? {get}
     var authenticationMethods: [String]? {get}
-	var isExpired: Bool {get}
+    var isExpired: Bool {get}
     var isAnonymous: Bool {get}
 }
 
 internal class AbstractToken: Token {
-	
-	private static let ISSUER = "iss"
-	private static let SUBJECT = "sub"
-	private static let AUDIENCE = "aud"
-	private static let EXPIRATION = "exp"
-	private static let ISSUED_AT = "iat"
-	private static let TENANT = "tenant"
-	private static let AUTH_METHODS = "amr"
-
-	var raw: String
-	var header: Dictionary<String, Any>
-	var payload: Dictionary<String, Any>
-	var signature: String
-	
-	internal init? (with raw: String) {
-		self.raw = raw
-		let tokenComponents = self.raw.components(separatedBy: ".")
-		guard tokenComponents.count==3 else {
-			return nil
-		}
-		
-		let headerComponent = tokenComponents[0]
-		let payloadComponent = tokenComponents[1]
-		self.signature = tokenComponents[2]
-
-		guard
-			let headerDecodedData = Utils.decodeBase64WithString(headerComponent, isSafeUrl: true),
-			let payloadDecodedData = Utils.decodeBase64WithString(payloadComponent, isSafeUrl: true)
-			else {
-				return nil
-		}
-		
-		guard
-			let headerDecodedString = String(data: headerDecodedData, encoding: String.Encoding.utf8),
-			let payloadDecodedString = String(data: payloadDecodedData, encoding: String.Encoding.utf8)
-			else {
-				return nil
-		}
-		
-		guard
-			let headerDictionary = try? Utils.parseJsonStringtoDictionary(headerDecodedString),
-			let payloadDictionary = try? Utils.parseJsonStringtoDictionary(payloadDecodedString)
-			else {
-				return nil
-		}
-		
-		self.header = headerDictionary
-		self.payload = payloadDictionary
-	}
-	
-	var issuer: String? {
-		return payload[AbstractToken.ISSUER] as? String
-	}
-
-	var subject: String? {
-		return payload[AbstractToken.SUBJECT] as? String
-	}
-	
-	var audience: String? {
-		return payload[AbstractToken.AUDIENCE] as? String
-	}
-	
+    
+    private static let ISSUER = "iss"
+    private static let SUBJECT = "sub"
+    private static let AUDIENCE = "aud"
+    private static let EXPIRATION = "exp"
+    private static let ISSUED_AT = "iat"
+    private static let TENANT = "tenant"
+    private static let AUTH_METHODS = "amr"
+    
+    var raw: String
+    var header: Dictionary<String, Any>
+    var payload: Dictionary<String, Any>
+    var signature: String
+    
+    internal init? (with raw: String) {
+        self.raw = raw
+        let tokenComponents = self.raw.components(separatedBy: ".")
+        guard tokenComponents.count==3 else {
+            return nil
+        }
+        
+        let headerComponent = tokenComponents[0]
+        let payloadComponent = tokenComponents[1]
+        self.signature = tokenComponents[2]
+        
+        guard
+            let headerDecodedData = Utils.decodeBase64WithString(headerComponent, isSafeUrl: true),
+            let payloadDecodedData = Utils.decodeBase64WithString(payloadComponent, isSafeUrl: true)
+            else {
+                return nil
+        }
+        
+        guard
+            let headerDecodedString = String(data: headerDecodedData, encoding: String.Encoding.utf8),
+            let payloadDecodedString = String(data: payloadDecodedData, encoding: String.Encoding.utf8)
+            else {
+                return nil
+        }
+        
+        guard
+            let headerDictionary = try? Utils.parseJsonStringtoDictionary(headerDecodedString),
+            let payloadDictionary = try? Utils.parseJsonStringtoDictionary(payloadDecodedString)
+            else {
+                return nil
+        }
+        
+        self.header = headerDictionary
+        self.payload = payloadDictionary
+    }
+    
+    var issuer: String? {
+        return payload[AbstractToken.ISSUER] as? String
+    }
+    
+    var subject: String? {
+        return payload[AbstractToken.SUBJECT] as? String
+    }
+    
+    var audience: [String]? {
+        return payload[AbstractToken.AUDIENCE] as? [String]
+    }
+    
     var expiration: Date? {
         guard let exp = payload[AbstractToken.EXPIRATION] as? Double else {
             return nil
@@ -94,14 +94,14 @@ internal class AbstractToken: Token {
         }
         return Date(timeIntervalSince1970: iat)
     }
-	var tenant: String? {
-		return payload[AbstractToken.TENANT] as? String
-	}
-	
+    var tenant: String? {
+        return payload[AbstractToken.TENANT] as? String
+    }
+    
     var authenticationMethods: [String]? {
         return payload[AbstractToken.AUTH_METHODS] as? [String]
     }
-	
+    
     var isExpired: Bool {
         guard let exp = self.expiration else {
             return true
@@ -116,5 +116,5 @@ internal class AbstractToken: Token {
         }
         return amr.contains("appid_anon")
     }
-	
+    
 }

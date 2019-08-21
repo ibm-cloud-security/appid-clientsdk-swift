076d029e7e1a7fc60ccbcc7fa9ca8abbf39fd38f
Rani Abboud <rani.abboud@ibm.com>
2019-08-21 13:47:10 +0300

    added a logout function in AppID class which wraps AppIDAuthorizationManager's logout.

--- Source/IBMCloudAppID/api/AppID.swift
+++ Source/IBMCloudAppID/api/AppID.swift
@@ -93,4 +93,8 @@ public class AppID {
             return (self.oauthManager?.authorizationManager?.application(application, open: url, options: options))!
     }
 
+    public func logout(){
+        let appIDAuthorizationManager = AppIDAuthorizationManager(appid: self)
+        appIDAuthorizationManager.logout()
+    }
 }
